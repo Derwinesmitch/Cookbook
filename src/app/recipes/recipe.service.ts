6f4e7d492dfc098452e54c0668936966e6610429
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2023-09-05 11:08:56 +0200

    added example recipe, added service for shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,22 +1,41 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
-      "A Test Recipe",
-      "This is a test desc",
-      "https://cdn.pixabay.com/photo/2017/04/08/14/37/kitchen-2213422_960_720.jpg"
+      "French Apple Cake",
+      "Perfect for kaffee und kuchen",
+      "https://www.allrecipes.com/thmb/1hg2zSq53KOtNHs-cT6Uv8FOlQc=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/7963451FrenchAppleCakeChefJohn4x3-20187dca9e214f569450a6f4d3ad219d.jpg",
+      [
+        new Ingredient("Baking Apple", 3),
+        new Ingredient("Cup of Flour", 1),
+        new Ingredient("Large Egg", 1),
+      ]
     ),
     new Recipe(
-      "A Test Recipe",
-      "This is a test desc",
-      "https://cdn.pixabay.com/photo/2017/04/08/14/37/kitchen-2213422_960_720.jpg"
+      "Caprese Salad",
+      "Light but Delicious",
+      "https://www.allrecipes.com/thmb/eE8Y1xRVY9nxwZ7UwRWp53WNrsw=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/1018869-400586536fcc4e77a22223465b65e095.jpg",
+      [
+        new Ingredient("Tomatoes", 3),
+        new Ingredient("Mozzarella Cheese", 1),
+        new Ingredient("Basil Leaves", 5),
+      ]
     ),
   ];
 
+  constructor(private slService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
