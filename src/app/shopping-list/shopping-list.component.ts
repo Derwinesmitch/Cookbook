755c2d91f69b0fa21194d10c0f146b8e9b102980
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2023-10-18 14:37:28 +0200

    created onEditItem method

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -20,6 +20,11 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
       }
     );
   }
+
+  onEditItem(index: number) {
+    this.slService.startedEditing.next(index);
+  }
+
   ngOnDestroy(): void {
     this.igChangeSub.unsubscribe();
   }
