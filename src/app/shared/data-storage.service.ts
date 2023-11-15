7fa787f0736a1eedd4eba239f75a4dd8e7079ad7
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2023-11-15 17:40:21 +0100

    added fetch recipes to get from database

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
-
+import { Recipe } from "../recipes/recipe.model";
 @Injectable({ providedIn: "root" })
 export class DataStorageService {
   constructor(private http: HttpClient, private recipeService: RecipeService) {}
@@ -15,6 +15,16 @@ export class DataStorageService {
       )
       .subscribe((response) => {
         console.log(response);
+      });
+  }
+
+  fetchRecipes() {
+    this.http
+      .get<Recipe[]>(
+        "https://cooking-recipe-project-default-rtdb.europe-west1.firebasedatabase.app/recipes.json"
+      )
+      .subscribe((recipes) => {
+        this.recipeService.setRecipes(recipes);
       });
   }
 }
