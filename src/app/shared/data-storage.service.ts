dd8ee62fdca64db5609e76b6100824319c210f8b
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2023-11-16 20:42:43 +0100

    added tap to fetchRecipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { RecipeService } from "../recipes/recipe.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map } from "rxjs/operators";
+import { map, tap } from "rxjs/operators";
 @Injectable({ providedIn: "root" })
 export class DataStorageService {
   constructor(private http: HttpClient, private recipeService: RecipeService) {}
@@ -20,7 +20,7 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http
+    return this.http
       .get<Recipe[]>(
         "https://cooking-recipe-project-default-rtdb.europe-west1.firebasedatabase.app/recipes.json"
       )
@@ -32,10 +32,10 @@ export class DataStorageService {
               ingredients: recipe.ingredients ? recipe.ingredients : [],
             };
           });
+        }),
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
         })
-      )
-      .subscribe((recipes) => {
-        this.recipeService.setRecipes(recipes);
-      });
+      );
   }
 }
