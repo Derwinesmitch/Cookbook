c620cc5c5336c57c544b39eca2975ff408467b41
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2023-11-16 11:39:46 +0100

    commented out dummy recipes/ created recipe array

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,29 +7,30 @@ import { ShoppingListService } from "../shopping-list/shopping-list.service";
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
-  private recipes: Recipe[] = [
-    new Recipe(
-      "French Apple Cake",
-      "Perfect for kaffee und kuchen",
-      "https://www.allrecipes.com/thmb/1hg2zSq53KOtNHs-cT6Uv8FOlQc=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/7963451FrenchAppleCakeChefJohn4x3-20187dca9e214f569450a6f4d3ad219d.jpg",
-      [
-        new Ingredient("Baking Apple", 3),
-        new Ingredient("Cup of Flour", 1),
-        new Ingredient("Large Egg", 1),
-      ]
-    ),
-    new Recipe(
-      "Caprese Salad",
-      "Light but Delicious",
-      "https://www.allrecipes.com/thmb/eE8Y1xRVY9nxwZ7UwRWp53WNrsw=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/1018869-400586536fcc4e77a22223465b65e095.jpg",
-      [
-        new Ingredient("Tomatoes", 3),
-        new Ingredient("Mozzarella Cheese", 1),
-        new Ingredient("Basil Leaves", 5),
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     "French Apple Cake",
+  //     "Perfect for kaffee und kuchen",
+  //     "https://www.allrecipes.com/thmb/1hg2zSq53KOtNHs-cT6Uv8FOlQc=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/7963451FrenchAppleCakeChefJohn4x3-20187dca9e214f569450a6f4d3ad219d.jpg",
+  //     [
+  //       new Ingredient("Baking Apple", 3),
+  //       new Ingredient("Cup of Flour", 1),
+  //       new Ingredient("Large Egg", 1),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     "Caprese Salad",
+  //     "Light but Delicious",
+  //     "https://www.allrecipes.com/thmb/eE8Y1xRVY9nxwZ7UwRWp53WNrsw=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/1018869-400586536fcc4e77a22223465b65e095.jpg",
+  //     [
+  //       new Ingredient("Tomatoes", 3),
+  //       new Ingredient("Mozzarella Cheese", 1),
+  //       new Ingredient("Basil Leaves", 5),
+  //     ]
+  //   ),
+  // ];
 
+  private recipes: Recipe[];
   constructor(private slService: ShoppingListService) {}
 
   setRecipes(recipes: Recipe[]) {
