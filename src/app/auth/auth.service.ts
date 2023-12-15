6212a1342534d37168d1b615ed14c84691edf8b4
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2023-12-15 13:10:07 +0100

    feat(auth): add logout method and redirect
    this commit adds a 'logout' method which sets user status to null and then redirects to auth landing page

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { catchError, tap } from "rxjs/operators";
 import { BehaviorSubject, throwError } from "rxjs";
 import { User } from "./user.model";
+import { Router } from "@angular/router";
 
 export interface AuthResponseData {
   kind: string;
@@ -18,7 +19,7 @@ export interface AuthResponseData {
 export class AuthService {
   user = new BehaviorSubject<User>(null);
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private router: Router) {}
 
   signUp(email: string, password: string) {
     return this.http
@@ -56,6 +57,13 @@ export class AuthService {
           );
         })
       );
+  }
+
+  // this method logs out the user by setting user status to null, after redirects to /auth
+
+  logout() {
+    this.user.next(null);
+    this.router.navigate(["/auth"]);
   }
 
   private handleAuthentication(
