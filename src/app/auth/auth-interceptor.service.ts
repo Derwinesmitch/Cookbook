54748ad2c3960fe5d764e67ca86872a3087e9491
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2023-12-15 12:33:26 +0100

    feat(auth): created auth-interceptor file
    this commit adds the interceptor which will add tokens for authentication to http requests

---
+++ src/app/auth/auth-interceptor.service.ts
@@ -0,0 +1,26 @@
+import { HttpHandler, HttpParams, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { HttpInterceptor } from "@angular/common/http";
+import { AuthService } from "./auth.service";
+import { take, exhaustMap } from "rxjs/operators";
+
+@Injectable()
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
+
+  // intercepts http request and adds auth token
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return this.authService.user.pipe(
+      take(1),
+      exhaustMap((user) => {
+        if (!user) {
+          return next.handle(req);
+        }
+        const modifiedReq = req.clone({
+          params: new HttpParams().set("auth", user.token),
+        });
+        return next.handle(modifiedReq);
+      })
+    );
+  }
+}
