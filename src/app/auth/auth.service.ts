cf70e50dbc10bfa0eb9669491450747ac9c3e78e
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2023-12-18 22:13:22 +0100

    feat(auth): enhance AuthService with autoLogin
    this commit adds the autoLogin method which logs in the user when the application starts by retrieving user data from local storage

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -59,6 +59,30 @@ export class AuthService {
       );
   }
 
+  // this method logs in the user when app starts via storage
+  autoLogin() {
+    const userData: {
+      email: string;
+      id: string;
+      _token: string;
+      _tokenExpirationDate: string;
+    } = JSON.parse(localStorage.getItem("userData"));
+    if (!userData) {
+      return;
+    }
+
+    const loadedUser = new User(
+      userData.email,
+      userData.id,
+      userData._token,
+      new Date(userData._tokenExpirationDate)
+    );
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser);
+    }
+  }
+
   // this method logs out the user by setting user status to null, after redirects to /auth
 
   logout() {
@@ -75,6 +99,7 @@ export class AuthService {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
     const user = new User(email, userId, token, expirationDate);
     this.user.next(user);
+    localStorage.setItem("userData", JSON.stringify(user));
   }
 
   private handleError(errorRes: HttpErrorResponse) {
