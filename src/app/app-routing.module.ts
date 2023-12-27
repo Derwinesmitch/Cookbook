7ee182fbc4809d7a593e02bcc49d647189da688c
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2023-12-27 17:07:12 +0100

    refactor(app-routing): revert lazy loading syntax
    this commit reverts a change made to the lazy loading which caused the app no not load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ const appRoutes: Routes = [
   { path: "", redirectTo: "/recipes", pathMatch: "full" },
   {
     path: "recipes",
-    loadChildren: () =>
-      import("./recipes/recipes.module").then((m) => m.RecipesModule),
+    loadChildren: "./recipes/recipes.module#RecipesModule",
   },
 ];
 
