95ce4f8de22c3f92d2b8abf3f73724ac562f287d
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2023-12-28 22:20:40 +0100

    feat(environment): add Firebase Key to develop environment
    This commit adds the Firebase API key to the development environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseApiKey: "AIzaSyCEXGoRrnogN-Sqs7BnHTHHg_wPsVNuCpk",
 };
 
 /*
