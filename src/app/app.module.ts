bdbe12dcb331da2dae92b74c7ad59a6ab9f75a5d
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2024-01-02 21:30:35 +0100

    feat: integrate Angular Material
    This commit adds the imports for BrowserAnimationsModule and also the MaterialModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HeaderComponent } from "./header/header.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { SharedModule } from "./shared/shared.module";
 import { CoreModule } from "./core.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material.module";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -15,6 +17,8 @@ import { CoreModule } from "./core.module";
     AppRoutingModule,
     SharedModule,
     CoreModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   bootstrap: [AppComponent],
 })
