b76b78bc42bd05783ce56775a7fe9108fc4b77fd
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2024-01-10 23:32:30 +0100

    feat(recipe-model): added steps to recipe model
    This commit adds the property steps to the recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -5,16 +5,19 @@ export class Recipe {
   public description: string;
   public imagePath: string;
   public ingredients: Ingredient[];
+  public steps: string;
 
   constructor(
     name: string,
     desc: string,
     imagePath: string,
-    ingredients: Ingredient[]
+    ingredients: Ingredient[],
+    steps: string
   ) {
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
     this.ingredients = ingredients;
+    this.steps = steps;
   }
 }
